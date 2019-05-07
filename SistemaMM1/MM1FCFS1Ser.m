37fc82453baee3d90e0729dfe6b1a46a0d71b123
acalvillm <acalvillm@gmail.com>
2019-05-07 07:01:14 -0500

    Reporte y algunos cambios

--- SistemaMM1/MM1FCFS1Ser.m
+++ SistemaMM1/MM1FCFS1Ser.m
@@ -5,40 +5,7 @@ function MM1FCFS1Ser(n,mi,lambda,x0,x1)
 %    mi: Media del tiempo de servicios 
 %lambda: Tasa de llegadas
 %x0--x1: Estado del sistema durante los servicios x1-x0
-
-%k al número de llegada
-k=1;
 close all;
-
-%tipoG determina si se generan tiempos de salida o de llegada
-% tipoG=1;
-% Ts=zeros(1,n);
-% Ti(1)=-log(rand())/lambda;
-% Ts(1)=-log(rand())/mi;
-% Te(1)=0;
-% Nt(1,:)=[0,0];
-% i=1;
-% while i<n
-% switch tipoG
-%     case 1
-%         if (Ti(k)>=Te(i))
-%             tipoG=2;
-%         else
-%             Ti(k+1)=Ti(k)-log(rand())/lambda;
-%             Nt(k+i,:)=[Nt(k+i-1,1)+1,Ti(k+1)];
-%             k=k+1;
-%         end
-%     case 2
-%         Ts(i+1)=-log(rand())/mi;
-%         tacum=cumsum(Ts);
-%         Te(i+1)=(tacum(i+1)-Ti(i))*(tacum(i+1)-Ti(i)>0);
-%         Nt(k+i,:)=[Nt(k+i-1,1)-((Nt(k+i-1,1))>0),Te(i+1)+Ti(i)];
-%         i=i+1;
-%         tipoG=1;
-% end
-% end
-% A=[Ti(1:n);Te+Ti(1:n);Ts;Te;Te+Ts]';
-
 %Tiempos de llegada
 Ti=-log(rand(1,n+1))/lambda;
 Ti=cumsum(Ti);
@@ -74,14 +41,24 @@ end
 N=Nt(x0:x1,:);
 stairs(N(:,1))
 for i=1:max(Nt(:,1))+1
-    aux=cumsum(Nt(:,1)==i-1);
-    P(i)=aux(length(Nt(:,1)));
+    aux1=find(Nt(:,1)==i-1);
+    if aux1(length(aux1))==length(Nt(:,1))
+        aux1=aux1(1:length(aux1)-1);
+   
+    end
+    aux2=aux1+1;
+    tsum=sum(Nt(aux2,2)-Nt(aux1,2));
+    P(i)=tsum;
 end
-P=P/length(Nt(:,1));
+P=P/max(Nt(:,2));
 figure(2)
-plot([0:1:max(Nt(:,1))],P)
-
+subplot(2,1,1)
+stem([0:1:max(Nt(:,1))],P,'ok')
+subplot(2,1,2)
+stem([0:1:max(Nt(:,1))],cumsum(P),'ok')
 %Verificación de Little
-j=trapz(Nt(1:min([i,k]),2),Nt(1:min([i,k]),1))/max(Nt(1:min([i,k]),2))
-d=mean(Te(1:min([i,k-1]))+Ts(1:min([i,k-1]))).*lambda;
-abs(1-j/d)
+N=trapz(Nt(1:min([i,k]),2),Nt(1:min([i,k]),1))/max(Nt(1:min([i,k]),2))
+T=mean(Te(1:min([i,k-1]))+Ts(1:min([i,k-1])))
+abs(N-T./lambda)
+P(1)
+P(2)
